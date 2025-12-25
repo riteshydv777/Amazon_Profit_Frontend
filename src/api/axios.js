f7a9f3aadc7704db424536859d19826ca9ce9130
Ritesh Kumar <mrritesh2508@gmail.com>
2025-12-25 16:29:33 +0530

    Fix CORS: remove withCredentials for JWT auth

--- src/api/axios.js
+++ src/api/axios.js
@@ -2,8 +2,7 @@ import axios from "axios";
 import { getToken } from "../utils/auth";
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE_URL,
-  withCredentials: true
+  baseURL: import.meta.env.VITE_API_BASE_URL
 });
 
 api.interceptors.request.use((config) => {
