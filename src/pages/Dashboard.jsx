009f70aca45855e72d6213db54ed5e03c59d5bc3
Ritesh Kumar <mrritesh2508@gmail.com>
2026-01-04 02:05:43 +0530

    Fix duplicate React hook imports

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,7 +1,4 @@
 import React, { useEffect, useState } from "react";
-
-
-import { useEffect, useState } from "react";
 import { getToken } from "../utils/auth";
 import axios from "axios";
 import DetailedProfitReport from "./DetailedProfitReport";
